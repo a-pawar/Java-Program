ceac77aead01262cf7b58da40441201005cf766d
Anshul Pawar <anshulpawar100@gmail.com>
2022-09-09 21:15:53 +0530

    Add files via upload

---
+++ Basic/GetTime.java
@@ -0,0 +1,8 @@
+//Write a Java program to display the system time.
+
+class  GetDateTime{
+	public static void main(String [] args){
+			System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+	}
+}
+		
