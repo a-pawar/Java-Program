4c786e25b937fd17256017a19698d2358b131041
Anshul Pawar <anshulpawar100@gmail.com>
2022-09-20 09:34:25 +0530

    Add files via upload

---
+++ Basic/clg-java-program/p5.java
@@ -0,0 +1,10 @@
+
+class Multiple{
+	public static void main(String [] args){
+		int a=20,b=10;
+		if(a%b == 0)
+			System.out.println("Multiple");
+		else
+			System.out.println("Not Multiple");
+	}
+}
